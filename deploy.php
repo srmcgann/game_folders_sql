97bd78ed69272175d988d102e7d0e7cddb6149c1
Scott R McGann <s.r.mcgann@hotmail.com>
2023-12-16 13:10:51 -0800

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -6,15 +6,15 @@ $sql = file_get_contents('https://srmcgann.github.io/game_folders_sql/game_folde
 
 if (mysqli_multi_query($link, $sql)) {
   do {
-    if ($res = mysqli_store_result()) {
+    if ($res = mysqli_store_result($link)) {
       while ($row = mysqli_fetch_row($res)) {
         printf("%s\n", $row[0]);
       }
-     mysqli_free_result();
+     mysqli_free_result($res);
     }
-    if (mysqli_more_results()) {
+    if (mysqli_more_results($link)) {
     }
-  } while (mysqli_next_result());
+  } while (mysqli_next_result($link));
 }
 
 mysqli_close($link);
