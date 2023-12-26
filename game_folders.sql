1d1cf85de8a92eb1d7a50ea782ecadd61ca1444c
Scott R McGann <s.r.mcgann@hotmail.com>
2023-12-26 11:37:38 -0800

    Update game_folders.sql

--- game_folders.sql
+++ game_folders.sql
@@ -7,6 +7,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+DROP TABLE orbsMirrors;
 
 CREATE TABLE `orbsMirrors` (
   `id` bigint(11) NOT NULL,
