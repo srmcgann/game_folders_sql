7adba2c2766a92ad20e225076bbfc84a3e456ebc
Scott R McGann <s.r.mcgann@hotmail.com>
2023-12-26 12:50:29 -0800

    Update deploy_to_000_hosts.sql

--- deploy_to_000_hosts.sql
+++ deploy_to_000_hosts.sql
@@ -42,7 +42,9 @@ INSERT INTO `orbsMirrors` (`id`, `topURL`, `actualURL`, `user`, `cred`, `server`
 (11, 'https://gummier-fish.000webhostapp.com', 'https://gummier-fish.000webhostapp.com/', 'id21257390_user', 'id21257390_default', 'localhost', 'Chrome57253!*', 1),
 (12, 'https://whr.rf.gd', 'https://whr.rf.gd/', 'if0_35615011', 'if0_35615011_orbs', 'sql101.infinityfree.com', 'ouVkeSu5FegeH', 0),
 (13, 'https://whr.42web.io', 'https://whr.42web.io/', 'if0_35680091', 'if0_35680091_arena', 'sql312.infinityfree.com', 'kjiGQM2DqnhUAuU', 0),
-(14, 'https://whr.000.pe', 'https://whr.000.pe/', 'if0_35680402', 'if0_35680402_arena', 'sql200.infinityfree.com', 'nBbQv0M3POyp', 0);
+(14, 'https://whr.000.pe', 'https://whr.000.pe/', 'if0_35680402', 'if0_35680402_arena', 'sql200.infinityfree.com', 'nBbQv0M3POyp', 0),
+(15, 'https://orb.42web.io', 'https://orb.42web.io/', 'if0_35680488', 'if0_35680488_arena', 'sql213.infinityfree.com', '9K12EE4mmF3yi', 0);
+
 
 
 
