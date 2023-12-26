91dc440ac177e6fd123b29576b94b3350bdd2c38
Scott R McGann <s.r.mcgann@hotmail.com>
2023-12-26 12:03:56 -0800

    Update deploy_to_000_hosts.sql

--- deploy_to_000_hosts.sql
+++ deploy_to_000_hosts.sql
@@ -41,7 +41,7 @@ INSERT INTO `orbsMirrors` (`id`, `topURL`, `actualURL`, `user`, `cred`, `server`
 (10, 'https://orbstools.000webhostapp.com', 'https://orbstools.000webhostapp.com/', 'id21594651_user', 'id21594651_orbstools', 'localhost', 'Chrome57253!*', 1),
 (11, 'https://gummier-fish.000webhostapp.com', 'https://gummier-fish.000webhostapp.com/', 'id21257390_user', 'id21257390_default', 'localhost', 'Chrome57253!*', 1),
 (12, 'https://whr.rf.gd', 'https://whr.rf.gd/', 'if0_35615011', 'if0_35615011_orbs', 'sql101.infinityfree.com', 'ouVkeSu5FegeH', 0),
-(13, 'https://whr.web42.io', 'https://whr.web42.io/', 'if0_35680091', 'if0_35680091_arena', 'sql312.infinityfree.com', 'kjiGQM2DqnhUAuU', 0);
+(13, 'https://whr.web42.io', 'https://whr.42web.io/', 'if0_35680091', 'if0_35680091_arena', 'sql312.infinityfree.com', 'kjiGQM2DqnhUAuU', 0);
 
 
 CREATE TABLE IF NOT EXISTS `platformGames` (
