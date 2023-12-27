20e7b2104ed6c40fffef072a94562e060fd4b112
scott mcgann <s.r.mcgann@hotmail.com>
2023-12-27 22:50:26 +0000

    sync

--- deploy_to_infinity_hosts.sql
+++ deploy_to_infinity_hosts.sql
@@ -44,7 +44,8 @@ INSERT INTO `orbsMirrors` (`id`, `topURL`, `actualURL`, `user`, `cred`, `server`
 (13, 'https://whr.42web.io', 'https://whr.42web.io/', 'if0_35680091', 'if0_35680091_arena', 'sql312.infinityfree.com', 'kjiGQM2DqnhUAuU', 1),
 (14, 'https://whr.000.pe', 'https://whr.000.pe/', 'if0_35680402', 'if0_35680402_arena', 'sql200.infinityfree.com', 'nBbQv0M3POyp', 1),
 (15, 'https://orb.42web.io', 'https://orb.42web.io/', 'if0_35680488', 'if0_35680488_arena', 'sql213.infinityfree.com', '9K12EE4mmF3yi', 1),
-(16, 'https://efx.rf.gd', 'https://efx.rf.gd/', 'if0_35681218', 'if0_35681218_arena', 'sql111.infinityfree.com', 'siann2ji7AGh', '1');
+(16, 'https://efx.rf.gd', 'https://efx.rf.gd/', 'if0_35681218', 'if0_35681218_arena', 'sql111.infinityfree.com', 'siann2ji7AGh', '1'),
+(17, 'https://efx.42web.io', 'https://efx.42web.io/', 'if0_35686192', 'if0_35686192_arena', 'sql106.infinityfree.com', 'iOFWM03Om1SRTI', 1);
 
 
 CREATE TABLE IF NOT EXISTS `platformGames` (
