eb8c64e7a62ebe555aa0dfa0c77bffef27316c14
Scott R McGann <s.r.mcgann@hotmail.com>
2023-12-27 11:06:53 -0800

    Create deploy_full_db_000.php

---
+++ deploy_full_db_000.php
@@ -0,0 +1,22 @@
+<?php
+
+require('db.php');
+
+$sql = file_get_contents('https://srmcgann.github.io/game_folders_sql/full_db_000.sql');
+
+if (mysqli_multi_query($link, $sql)) {
+  do {
+    if ($res = mysqli_store_result($link)) {
+      while ($row = mysqli_fetch_row($res)) {
+        printf("%s\n", $row[0]);
+      }
+     mysqli_free_result($res);
+    }
+    if (mysqli_more_results($link)) {
+    }
+  } while (mysqli_next_result($link));
+}
+
+mysqli_close($link);
+
+echo "\n\ndone.\n\n";
