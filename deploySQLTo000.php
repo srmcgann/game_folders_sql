c7cc044736e2a3cf62192f1e855a6a5d244007b8
Scott R McGann <s.r.mcgann@hotmail.com>
2024-03-21 11:44:46 -0700

    Add files via upload

---
+++ deploySQLTo000.php
@@ -0,0 +1,21 @@
+<?php
+
+require('db.php');
+
+$sql = file_get_contents('https://srmcgann.github.io/game_folders_sql/000Games.sql');
+
+if (mysqli_multi_query($link, $sql)) {
+  do {
+    if ($result = mysqli_store_result($link)) {
+      while ($row = mysqli_fetch_row($result)) {
+        printf("%s\n", $row[0]);
+      }
+      mysqli_free_result($result);
+    }
+    if (mysqli_more_results($link)) {
+      printf("-------------\n");
+    }
+  } while (mysqli_next_result($link));
+}
+
+?>
